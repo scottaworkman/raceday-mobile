629b1aa93dd837764c18d5a1683751d94505700f
Scott Workman <scott@workmanfamily.com>
2018-04-02 12:08:11 -0400

    updating bash script

--- RaceDay/RaceDay.Droid/appcenter-post-clone.sh
+++ RaceDay/RaceDay.Droid/appcenter-post-clone.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sed -i "s/RACEDAY_GROUP_NAME/$RACEDAY_GROUP_NAME/g" $APPCENTER_SOURCE_DIRECTORY/RaceDay/RaceDay/Helpers/SettingsDefaults.cs
-sed -i "s/RACEDAY_GROUP_CODE/$RACEDAY_GROUP_CODE/g" $APPCENTER_SOURCE_DIRECTORY/RaceDay/RaceDay/Helpers/SettingsDefaults.cs
-sed -i "s/RACEDAY_FACEBOOK_GROUP/$RACEDAY_FACEBOOK_GROUP/g" $APPCENTER_SOURCE_DIRECTORY/RaceDay/RaceDay/Helpers/SettingsDefaults.cs
-sed -i "s/RACEDAY_GROUP_API/$RACEDAY_GROUP_API/g" $APPCENTER_SOURCE_DIRECTORY/RaceDay/RaceDay/Helpers/SettingsDefaults.cs
-sed -i "s/RACEDAY_FACEBOOK_APP/$RACEDAY_FACEBOOK_APP/g" $APPCENTER_SOURCE_DIRECTORY/RaceDay/RaceDay/Helpers/SettingsDefaults.cs
+sed "s/RACEDAY_GROUP_NAME/$RACEDAY_GROUP_NAME/g" -i $APPCENTER_SOURCE_DIRECTORY/RaceDay/RaceDay/Helpers/SettingsDefaults.cs
+sed "s/RACEDAY_GROUP_CODE/$RACEDAY_GROUP_CODE/g" -i $APPCENTER_SOURCE_DIRECTORY/RaceDay/RaceDay/Helpers/SettingsDefaults.cs
+sed "s/RACEDAY_FACEBOOK_GROUP/$RACEDAY_FACEBOOK_GROUP/g" -i $APPCENTER_SOURCE_DIRECTORY/RaceDay/RaceDay/Helpers/SettingsDefaults.cs
+sed "s/RACEDAY_GROUP_API/$RACEDAY_GROUP_API/g" -i $APPCENTER_SOURCE_DIRECTORY/RaceDay/RaceDay/Helpers/SettingsDefaults.cs
+sed "s/RACEDAY_FACEBOOK_APP/$RACEDAY_FACEBOOK_APP/g" -i $APPCENTER_SOURCE_DIRECTORY/RaceDay/RaceDay/Helpers/SettingsDefaults.cs
 
-sed -p $APPCENTER_SOURCE_DIRECTORY/RaceDay/RaceDay/Helpers/SettingsDefaults.cs
+cat $APPCENTER_SOURCE_DIRECTORY/RaceDay/RaceDay/Helpers/SettingsDefaults.cs
