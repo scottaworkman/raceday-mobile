0619c9a1eaf535a8c55a83ab621bbd8bb4c07d0f
Scott Workman <scott@workmanfamily.com>
2019-05-01 21:20:46 -0400

    Building iOS app

--- RaceDay/RaceDay.iOS/appcenter-post-clone.sh
+++ RaceDay/RaceDay.iOS/appcenter-post-clone.sh
@@ -5,14 +5,13 @@ namespace RaceDay.Helpers
 {
     public static class SettingsDefaults
     {
-        public static readonly string GroupNameDefault = "$RACEDAY_GROUP_NAME";
+		public static readonly string ApiUrlDefault = "$RACEDAY_API_URL";
+        public static readonly string GroupNameDefault = "$RACEAY_GROUP_NAME";
         public static readonly string GroupCodeDefault = "$RACEDAY_GROUP_CODE";
-        public static readonly string GroupFacebookIdDefault = "$RACEDAY_FACEBOOK_GROUP";
         public static readonly string GroupApiDefault = "$RACEDAY_GROUP_API";
         public static readonly string UserIdDefault = string.Empty;
         public static readonly string UserNameDefault = "Your Account";
         public static readonly string UserEmailDefault = string.Empty;
-        public static readonly string FacebookAppIdDefault = "$RACEDAY_FACEBOOK_APP";
     }
 }
 EOT
